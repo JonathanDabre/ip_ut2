5e12fe5639a712c0ef634932ea1d1ca417502c93
JonathanDabre <jonathandabre01@gmail.com>
2023-12-02 15:49:12 +0530

    explaination

--- ip-sem-prep/php_xml/formvalidation.php
+++ ip-sem-prep/php_xml/formvalidation.php
@@ -12,17 +12,27 @@ function sanitizeInput($data) {
 }
 
 // Check if the form is submitted
+// $_SERVER =>superglobal array in PHP that provides information about the server
+// $_POST => superglobal array in php used to collect form data submitted with the HTTP POST method
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate name and email using our sanitizeInput function 
     $name = sanitizeInput($_POST["name"]);
     $email = sanitizeInput($_POST["email"]);
 
-    // Check if the name contains only letters and whitespace
+    // Check if the name contains only letters 
+    // preg_match => pattern matching function in php
+    // /^[a-zA-Z ]*$/  => regular expression for indicating all combination of a-z & A-Z alphabhets
+    // /^ => start of pattern,  / => end of the pattern. $ => asserts end of the string
+
+
     if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
         $nameErr = "Only letters and white space allowed";
     }
     
-    // Check if the email address is well-formed
+    // Check if the email address is in valid format
+    // filter_var() => function to check validity of a variable in php. FILTER_VALIDATE_EMAIL => validate for email
+    
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $emailErr = "Invalid email format";
     }
